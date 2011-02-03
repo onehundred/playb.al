133e24ee67131109f87e115525548a175f0b399c
lenny <lenfrag@hotmail.com>
2011-02-03 11:45:25 +0100

    jquery versie geüpdate naar 1.5! javascript net voor </body> inladen header, footer aangepast.

--- eindwerk/application/views/includes/footer.php
+++ eindwerk/application/views/includes/footer.php
@@ -1,4 +1,13 @@
 <footer>
 </footer>
+<!-- todo jquery CDN veranderen van microsoft naar google,  google url not live at time of implementation. -->
+<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.5.min.js"></script>
+<script src="<?php echo base_url();?>js/formToWizard.js"></script>
+<script>
+        $(document).ready(function(){
+            $("#SignupForm").formToWizard({ submitButton: 'submit' })
+        });
+        
+     </script>
 </body>
 </html>
--- eindwerk/application/views/includes/header.php
+++ eindwerk/application/views/includes/header.php
@@ -5,13 +5,5 @@
 <title>playb.al</title>
 <link rel="stylesheet" href="<?php echo base_url();?>css/main_login.css" />
 <link rel="stylesheet" href="<?php echo base_url();?>css/sport_signup.css" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-<script src="<?php echo base_url();?>js/formToWizard.js"></script>
-<script>
-        $(document).ready(function(){
-            $("#SignupForm").formToWizard({ submitButton: 'submit' })
-        });
-        
-     </script>
 </head>
 <body>

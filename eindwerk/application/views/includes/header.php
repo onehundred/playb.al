6d91411afa0237a071f72bf0f722ae5246fb60f6
Dimitri <Dimitri_leonidas@hotmail.com>
2011-02-03 14:21:19 +0100

    1.5

--- eindwerk/application/views/includes/header.php
+++ eindwerk/application/views/includes/header.php
@@ -5,7 +5,7 @@
 <title>playball</title>
 <link rel="stylesheet" href="<?php echo base_url();?>css/main_login.css" />
 <link rel="stylesheet" href="<?php echo base_url();?>css/sport_signup.css" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 <script src="<?php echo base_url();?>js/formToWizard.js"></script>
 <script>
         $(document).ready(function(){

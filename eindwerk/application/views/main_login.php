c456a04a62de436110b00af9edec4e132d5a5944
lenny <lenfrag@hotmail.com>
2011-02-03 16:40:04 +0100

    fontface implementatie css reset lessframework

--- eindwerk/application/views/main_login.php
+++ eindwerk/application/views/main_login.php
@@ -1,6 +1,14 @@
+<style type="text/css">
+h1 {
+	font-family: 'LeagueGothicRegular';
+}
+h2 {
+	font-family: 'SFCollegiateSolidRegular';
+}
+</style>
 <div id="main_login">
-	<h1>Login</h1>
-	<?php
+    <h1>Login</h1>
+    <?php
 		echo form_open('main/login');
 		echo form_input('username', 'Username');
 		echo form_password('password', 'Password');
@@ -11,11 +19,4 @@
 		echo anchor('main/signup','Create account')
 		
 		 ?>
-		
-		
-	
-	
-	
-
-
 </div>
--- eindwerk/application/views/main_sportkeuze.php
+++ eindwerk/application/views/main_sportkeuze.php
@@ -1,15 +1,18 @@
 <div id="main_sportkeuze">
-<h2>Korfbal</h2>
-<p><?php if(isset($korfNaam)){
+    <h2>Korfbal</h2>
+    <p>
+        <?php if(isset($korfNaam)){
 	echo anchor("korfbal/korfbal_start/$korfId", $korfNaam);
 }else{
 	
 	echo anchor('sportchoice/korfbal_signup','Create korfball team');
 
 
-} ?></p>
-<h2>Volleybal</h2>
-<p><?php if(isset($volNaam)){
+} ?>
+    </p>
+    <h2>Volleybal</h2>
+    <p>
+        <?php if(isset($volNaam)){
 		echo $volNaam;
 		echo $volId;
 
@@ -18,9 +21,11 @@
 	
 
 
-} ?></p>
-<h2>Basketbal</h2>
-<p><?php if(isset($basNaam)){
+} ?>
+    </p>
+    <h2>Basketbal</h2>
+    <p>
+        <?php if(isset($basNaam)){
 	
 		echo $basNaam;
 		echo $basId;
@@ -29,7 +34,6 @@
 	echo anchor('sportchoice/basketbal_signup','Create basketball team');
 	
 
-} ?></p>
-
-
+} ?>
+    </p>
 </div>

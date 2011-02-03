0c378928b3e343a5da0eec6f6f16312e519a56d4
lenny <lenfrag@hotmail.com>
2011-02-03 11:20:08 +0100

    type="javascript/text"  ==> niet nodig in html5

--- eindwerk/application/views/includes/header.php
+++ eindwerk/application/views/includes/header.php
@@ -2,14 +2,13 @@
 <html lang="en">
 <head>
 <meta charset="utf-8" />
-<title>playball</title>
-<link rel="stylesheet" href="<?php echo base_url();?>css/main_login.css" />
 <title>playb.al</title>
 <link rel="stylesheet" href="<?php echo base_url();?>css/main_login.css" />
+<link rel="stylesheet" href="<?php echo base_url();?>css/main_login.css" />
 <link rel="stylesheet" href="<?php echo base_url();?>css/sport_signup.css" />
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>js/formToWizard.js"></script>
-<script type="text/javascript">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
+<script src="<?php echo base_url();?>js/formToWizard.js"></script>
+<script>
         $(document).ready(function(){
             $("#SignupForm").formToWizard({ submitButton: 'submit' })
         });

ff033fa4b672c5b4493437bd5f862c28b181ebb8
lenny <lenfrag@hotmail.com>
2011-02-03 19:50:16 +0100

    header js en less volgorde

--- eindwerk/application/views/includes/header.php
+++ eindwerk/application/views/includes/header.php
@@ -3,7 +3,9 @@
 <head>
 <meta charset="utf-8" />
 <title>playb.al</title>
-<link rel="stylesheet" href="<?php echo base_url();?>css/main_login.css" />
-<link rel="stylesheet" href="<?php echo base_url();?>css/sport_signup.css" />
+<script src="<?php echo base_url();?>js/modernizr-1.6.min.js"></script>
+<link rel="stylesheet/less" href="<?php echo base_url();?>css/main_login.less" />
+<link rel="stylesheet/less" href="<?php echo base_url();?>css/sport_signup.less" />
+<script src="<?php echo base_url();?>js/less-1.0.41.min.js"></script> 
 </head>
 <body>

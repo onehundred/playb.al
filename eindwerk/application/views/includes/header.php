a625734ad4cdf1e96fd3bc77f18d9a855e385f4c
lenny <lenfrag@hotmail.com>
2011-02-03 11:27:34 +0100

    header gewijzigd

--- eindwerk/application/views/includes/header.php
+++ eindwerk/application/views/includes/header.php
@@ -4,7 +4,6 @@
 <meta charset="utf-8" />
 <title>playb.al</title>
 <link rel="stylesheet" href="<?php echo base_url();?>css/main_login.css" />
-<link rel="stylesheet" href="<?php echo base_url();?>css/main_login.css" />
 <link rel="stylesheet" href="<?php echo base_url();?>css/sport_signup.css" />
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
 <script src="<?php echo base_url();?>js/formToWizard.js"></script>

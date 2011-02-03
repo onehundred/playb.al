aeb99ff7437eec01fa2e94a78eba9d5936e20662
lenny <lenfrag@hotmail.com>
2011-02-03 19:44:45 +0100

    javascript in footer

--- eindwerk/application/views/includes/footer.php
+++ eindwerk/application/views/includes/footer.php
@@ -1,4 +1,11 @@
-<footer>
-</footer>
+<!--todo microsoft cdn veranderen naar google cdn (url niet live op moment van productie)-->
+<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.5.min.js"></script>
+<script src="<?php echo base_url();?>js/formToWizard.js"></script>
+<script>
+        $(document).ready(function(){
+            $("#SignupForm").formToWizard({ submitButton: 'submit' })
+        });
+</script>
+<footer></footer>
 </body>
 </html>

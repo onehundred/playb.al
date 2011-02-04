840fab1b9fe747b60c45b7c841fed0ebae94c2e8
Dimitri <Dimitri_leonidas@hotmail.com>
2011-02-04 20:08:41 +0100

    .less

--- eindwerk/application/config/config.php
+++ eindwerk/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8888/eindwerk/eindwerk/';
+$config['base_url']	= 'http://playb.al/eindwerk/';
 
 /*
 |--------------------------------------------------------------------------

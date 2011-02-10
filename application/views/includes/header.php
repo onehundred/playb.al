96aefe63e43fa95ffe42286ed9cde716f5619eb6
lenny <lenfrag@hotmail.com>
2011-02-10 14:23:41 +0100

    standaard wijzigingen

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,21 +1,17 @@
+<div id="topbar"></div>
 <div id="wrap">
-<header>
-<!-- todo logo, profiel, login -->
+<div id="main">
+	<header>
+	<!-- todo logo, profiel, login -->
+		<figure>
+			<a href="<?php echo base_url();?>">
+			<img src="<?php echo base_url();?>img/logo.png" id="logo" />
+			</a>
+		</figure>
+			<section>
+				<?php
+				$this->load->view('main_login');
+				?>
+			</section>
+	</header>
 
-<figure>
-
-<img src="<?php echo base_url();?>img/logo.png" id="logo" />
-
-
-</figure>
-
-
-
-
-<?php
-$this->load->view('main_login');
-?>
-
-
-</header>
-<div id="cont">	
---
+++ application/views/main_index.php
@@ -0,0 +1,11 @@
+<section id="layouts">
+	<figure id="korfbal">
+		<h1>korfbal</h1>
+	</figure>
+	<figure id="basketbal">
+		<h1>basketbal</h1>
+	</figure>
+	<figure id="volleybal">
+		<h1>volleybal</h1>
+	</figure>
+</section>

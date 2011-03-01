56a56b5938367ca4e9fa164bb07e940da638dc8c
lenny <lenfrag@hotmail.com>
2011-03-01 20:28:28 +0100

    major css update divided 1 stylesheet into 5

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -1,4 +1,3 @@
-
 <!-- head voor homepage -->
 
 <!DOCTYPE HTML>
@@ -7,16 +6,26 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width; initial-scale=1"/>
 <meta name="viewport" content="width=device-width; maximum-scale=1"/>
-
 <title>playb.al</title>
-
 <link rel="stylesheet" media="all" href=""/>
 <link href="<?php echo base_url();?>css/style.less" rel="stylesheet/less" />
 
+<!-- Fallback if browser does not support media queries + javascript (Read: Internet Explorer < 9) -->
+<!--[if lt IE 9]>
+	<link rel="stylesheet" href="8col.css" media="screen" />
+	<![endif]-->
 
-<script src="<?php echo base_url();?>js/less-1.0.41.min.js"></script> 
+<!-- Media Queries / Less -->
+<link href="<?php echo base_url();?>css/style1168.less" rel="stylesheet/less" media="only screen and (min-width: 1212px)" />
+<link href="<?php echo base_url();?>css/style768.less"  rel="stylesheet/less" media="only screen and (max-width: 991px) and (min-width: 768px)" />
+<link href="<?php echo base_url();?>css/style480.less"  rel="stylesheet/less" media="only screen and (max-width: 767px) and (min-width: 480px)" />
+<link href="<?php echo base_url();?>css/style320.less"  rel="stylesheet/less" media="only screen and (max-width: 479px)" />
+
+<script src="<?php echo base_url();?>js/less-1.0.41.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-<!-- <script src="<?php echo base_url();?>js/modernizr-1.6.min.js"></script>-->
+<script src="<?php echo base_url();?>js/jquery.mediaqueries.js"></script>
+<script src="<?php echo base_url();?>js/modernizr-1.6.min.js"></script>
+
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->

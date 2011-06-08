bc096f01dec7102a7ce11c7293b064efc4293ab2
lenny <lenfrag@hotmail.com>
2011-06-08 18:51:40 +0200

    style

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,16 +1,17 @@
 <div id="wrap">
 <div id="main">
-<div id="headerWrap">
+
     <header>
+    <div id="header">
         <figure> <a onclick="parent.location='<?php echo base_url();?>'">
             <img src="<?php echo base_url();?>img/1168/logo.png" id="logo" alt="logo" title="playb.al" ondragstart="return false" />
             </a> </figure>
         <section>
             <?php $this->load->view('main_login'); ?>
         </section>
+        </div>
     </header>
-</div>
-<!-- end headerWrap --> 
+
 <p id="jsnotice">
   Javascript is currently disabled. This site requires Javascript to function correctly.
   Please <a href="http://enable-javascript.com/"> enable Javascript in your browser</a>!

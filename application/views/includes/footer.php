f03e3473c2786c8176582ac863adb92d8bbd49ae
lenny <lenfrag@hotmail.com>
2011-06-10 20:45:42 +0200

    images for social networks

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -37,7 +37,7 @@
     <script type="text/javascript"> 
      $(document).ready(function(){
   		  $.jTwitter('justin', function(userdata){
-  		  	alert(userdata[0].text);
+  		  	//alert(userdata[0].text);
 			  //Callback functn with the user data as shown above
 			  $('#twitter').append(userdata[0].text);
 			  

15b6ccce55d384a21030a32db1695c41eac2a30d
Dimitri <Dimitri_leonidas@hotmail.com>
2011-06-11 16:45:28 +0200

    bugske

--- js/korfbal/financien.js
+++ js/korfbal/financien.js
@@ -135,14 +135,26 @@ $(document).ready(function()
     							dataType: "json",
         						success: function(data){
         						
+        							var finances = [];
+        							var weken = [];
+        							
+        							for(var i = 1 in data){
+        								finances = data[i];
+        								weken = "Week"+i;
+        							
+        							}
+        							
+        							
         							var week1 = data[1];
         							//alert(week1);
 									if(!!document.createElement('canvas').getContext){ //check that the canvas
 						                                                           // element is supported
 						            var mychart = new AwesomeChart('canvas1');
 						         	mychart.chartType = "horizontal bars";
-						            mychart.data = [data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13],data[14]];
-						            mychart.labels = ["Week1", "Week2", "Week3", "week4", "week5", "week6", "week7", "week8", "week9", "week10", "week11", "week12", "week13", "week14"];
+						         	 mychart.data = finances;
+						         	 mychart.labels = weken;
+						           // mychart.data = [data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14]];
+						            //mychart.labels = ["Week1", "Week2", "Week3", "week4", "week5", "week6", "week7", "week8", "week9", "week10", "week11", "week12", "week13", "week14"];
 						            mychart.draw();
 						        }
         

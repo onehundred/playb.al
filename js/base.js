0ed211825c0702787c1a2a5fbd9b9d84600dfa18
lenny <lenfrag@hotmail.com>
2011-06-15 22:11:14 +0200

    responsive

--- js/base.js
+++ js/base.js
@@ -1,6 +1,60 @@
 // playb.al
 
 
+/////////////////////////////////////////////////////////////////
+// ZWART WIT NAAR KLEUR begin
+/////////////////////////////////////////////////////////////////
+	$(window).load(function(){
+		
+		// images infaden voor flash te vermijden
+		$('#korfbalbg, #basketbalbg, #volleybalbg').fadeIn(500);
+		
+		// clone image
+		$('#korfbalbg, #basketbalbg, #volleybalbg').each(function(){
+			var el = $(this);
+			el.css({"position":"absolute"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position":"absolute","z-index":"2","opacity":"0"}).insertBefore(el).queue(function(){
+				var el = $(this);
+				el.parent().css({"width":this.width,"height":this.height});
+				el.dequeue();
+			});
+			this.src = grayscale(this.src);
+		});
+		
+		// Fade image 
+		$('#korfbalbg, #basketbalbg, #volleybalbg').mouseover(function(){
+			$(this).parent().find('img:first').stop().animate({opacity:1}, 100);
+		})
+
+		$('.img_grayscale').mouseout(function(){
+			$(this).stop().animate({opacity:0}, 1600);
+		});		
+	});
+	
+	// z-w canvas methode
+	function grayscale(src){
+		var canvas = document.createElement('canvas');
+		var ctx = canvas.getContext('2d');
+		var imgObj = new Image();
+		imgObj.src = src;
+		canvas.width = imgObj.width;
+		canvas.height = imgObj.height; 
+		ctx.drawImage(imgObj, 0, 0); 
+		var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		for(var y = 0; y < imgPixels.height; y++){
+			for(var x = 0; x < imgPixels.width; x++){
+				var i = (y * 4) * imgPixels.width + x * 4;
+				var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
+				imgPixels.data[i] = avg; 
+				imgPixels.data[i + 1] = avg; 
+				imgPixels.data[i + 2] = avg;
+			}
+		}
+		ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
+		return canvas.toDataURL();
+    }	
+/////////////////////////////////////////////////////////////////
+// ZWART WIT NAAR KLEUR end
+
 
 
 
@@ -194,60 +248,6 @@
 $(document).ready(function(){
 	$('.korfbal, .basketbal, .volleybal').fadeIn(500);
 });
-/////////////////////////////////////////////////////////////////
-// ZWART WIT NAAR KLEUR begin
-/////////////////////////////////////////////////////////////////
-	$(window).load(function(){
-		
-		// images infaden voor flash te vermijden
-		$('#korfbalbg, #basketbalbg, #volleybalbg').fadeIn(500);
-		
-		// clone image
-		$('#korfbalbg, #basketbalbg, #volleybalbg').each(function(){
-			var el = $(this);
-			el.css({"position":"absolute"}).wrap("<div class='img_wrapper' style='display: inline-block'>").clone().addClass('img_grayscale').css({"position":"absolute","z-index":"2","opacity":"0"}).insertBefore(el).queue(function(){
-				var el = $(this);
-				el.parent().css({"width":this.width,"height":this.height});
-				el.dequeue();
-			});
-			this.src = grayscale(this.src);
-		});
-		
-		// Fade image 
-		$('#korfbalbg, #basketbalbg, #volleybalbg').mouseover(function(){
-			$(this).parent().find('img:first').stop().animate({opacity:1}, 100);
-		})
-
-		$('.img_grayscale').mouseout(function(){
-			$(this).stop().animate({opacity:0}, 1600);
-		});		
-	});
-	
-	// z-w canvas methode
-	function grayscale(src){
-		var canvas = document.createElement('canvas');
-		var ctx = canvas.getContext('2d');
-		var imgObj = new Image();
-		imgObj.src = src;
-		canvas.width = imgObj.width;
-		canvas.height = imgObj.height; 
-		ctx.drawImage(imgObj, 0, 0); 
-		var imgPixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
-		for(var y = 0; y < imgPixels.height; y++){
-			for(var x = 0; x < imgPixels.width; x++){
-				var i = (y * 4) * imgPixels.width + x * 4;
-				var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-				imgPixels.data[i] = avg; 
-				imgPixels.data[i + 1] = avg; 
-				imgPixels.data[i + 2] = avg;
-			}
-		}
-		ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixels.width, imgPixels.height);
-		return canvas.toDataURL();
-    }	
-/////////////////////////////////////////////////////////////////
-// ZWART WIT NAAR KLEUR end
-
 /////////////////////////////////////////////////////////////////
 // HOMEPAGE ANIMATIONS begin
 /////////////////////////////////////////////////////////////////
